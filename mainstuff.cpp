9139596760cabc3503b77dabdc849f1e808aad61
thebearslicer <70990720+thebearslicer@users.noreply.github.com>
2020-09-08 22:34:04 -0400

    Add files via upload

---
+++ mainstuff.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+	char op; //var that stores operator
+
+	float num1, num2; //numbers
+	
+	cout << "Enter op (+,-,*,/,^,sqrt) \n"; //get operators 
+	cin >> op; 
+
+	cout << "Enter two numbers \n";
+	cin >> num1 >> num2; //get numbers
+	
+	switch (op) 
+	{
+	case '+':
+		cout << num1 + num2;
+		break;
+
+	case '-':
+		cout << num1 - num2;
+		break;
+
+	case '*':
+		cout << num1 * num2;
+		break;
+
+	case '/':
+
+		if (num2 != 0) {
+			cout << num1 / num2;
+			break;
+		}
+						//check for div by 0
+		else {
+			cout << "Cannot divide by 0!";
+			break;
+		}
+
+	case '^':
+		cout << pow(num1, num2);
+		break;
+
+	case 'sqrt':
+		cout << sqrt(num1);
+		break;
+
+
+	default:
+		cout << "The operator is incorrect";
+		break;
+
+
+	
+	}
+
+	return 0; 
+
+}
